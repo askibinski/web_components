0ab05c30fea4efe26bdcbdbb13da37c36fa86c7a
askibinski <albert.skibinski@intracto.com>
2020-04-06 10:52:38 +0200

    lit-html

--- example-data-binding.js
+++ example-data-binding.js
@@ -1,36 +1,11 @@
-// Modern browsers now support importing modules.
-// https://caniuse.com/#feat=es6-module
-import CustomElement from "./custom-element.js";
+import {html, render} from './node_modules/lit-html/lit-html.js';
 
-class MyElement extends CustomElement {
+// Define a template
+// NOTE: this is not yet a custom element. We would need lit-element for that.
+const myTemplate = (data) => html`
+  <h1>${data.title}</h1>
+  <div>${data.body}</div>
+`;
 
-  constructor() {
-    super();
-
-    const shadowRoot = this.attachShadow({mode: 'open'});
-
-    shadowRoot.innerHTML = `      
-      <style>
-        h1 {
-          color: blue;
-        }
-        div {
-          color: red;
-        }
-      </style>
-      <h1 data-bind="title"></h1>
-      <div data-bind="body"></div>
-    `;
-  }
-  
-}
-
-window.customElements.define('my-element', MyElement);
-const el = customElements.get('my-element');
-
-// Create a new element and bind data to the view.
-const myElement = new el(); 
-myElement.setState({title: 'Hello World', body: 'Lorem ipsum'});
-
-// Add the element to the page.
-document.body.appendChild(myElement);
+// Render the template to the document
+render(myTemplate({ title: 'Hello world', body: 'Lorem ipsum'}), document.body);
